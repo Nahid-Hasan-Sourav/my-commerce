f5316dd957906987f4f2cba2d35ca386f65bda79
“sourav” <sourav@excelitai.com>
2023-07-16 18:20:11 +0600

    added jetstream

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,12 @@ Route::get('/product-category',[MyCommerceController::class,'category'])->name('
 Route::get('/product-detail',[MyCommerceController::class,'detail'])->name('product-detail');
 Route::get('/show-cart',[CartController::class,'index'])->name('show-cart');
 Route::get('/checkout',[CheckoutController::class,'index'])->name('checkout');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
