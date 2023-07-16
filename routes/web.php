6d10843006c313be96cf7983a151966ef0e43cb3
“sourav” <sourav@excelitai.com>
2023-07-16 16:25:14 +0600

    added add to cart static page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MyCommerceController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[MyCommerceController::class,'index'])->name('home');
 Route::get('/product-category',[MyCommerceController::class,'category'])->name('product-category');
 Route::get('/product-detail',[MyCommerceController::class,'detail'])->name('product-detail');
+Route::get('/show-cart',[CartController::class,'index'])->name('show-cart');
